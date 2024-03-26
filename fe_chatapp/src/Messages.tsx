e0a86e0453539fdaeae390a8f265d7238281fb9a
jacksine01 <156636808+jacksine01@users.noreply.github.com>
2024-03-26 12:21:21 +0530

    fe-chatapp

---
+++ fe_chatapp/src/Messages.tsx
@@ -0,0 +1,13 @@
+function Messages({ messages }: { messages: string[] }) {
+  return (
+    <>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Messages;
